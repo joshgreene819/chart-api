0ebf596a86fca5f023b8d6c0a13940e831c6d3aa
joshgreene819 <josh.greene819@gmail.com>
2022-07-01 20:52:06 -0400

    Condense nested structs in models package

--- models/models.go
+++ models/models.go
@@ -2,19 +2,17 @@ package models
 
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
-// Provides configuration for how a dataset and its keys can be compliant with
-// a DatasetTemplate
-type Metadata struct {
-	AnyDepth       bool `json:"anyDepth"`
-	AssignDefaults bool `json:"assignDefaults"`
-	StrictMatch    bool `json:"strictMatch"`
-}
-
 // Keys that a DatasetTemplate will require in order to be implemented by a
-// dataset
+// dataset.
 type RequiredKeys struct {
-	Metadata Metadata               `json:"metadata"`
-	Data     map[string]interface{} `json:"data"`
+	// Provides configuration for how a dataset and its keys can be compliant with
+	// a DatasetTemplate
+	Metadata struct {
+		AnyDepth       bool `json:"anyDepth"`
+		AssignDefaults bool `json:"assignDefaults"`
+		StrictMatch    bool `json:"strictMatch"`
+	} `json:"metadata"`
+	Data map[string]interface{} `json:"data"`
 }
 
 // Definition of keys that a Dataset will need in order to comply with the
